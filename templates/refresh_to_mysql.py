e3a30957317518aa652c168861525e53d2a01c4a
TomNewChao <353712216@qq.com>
2022-12-20 18:54:09 +0800

    增加刷新模板的内容

--- templates/refresh_to_mysql.py
+++ templates/refresh_to_mysql.py
@@ -7,13 +7,14 @@
 import os
 import pymysql
 import click
+import traceback
 
 
 class GlobalConfig:
     GITHUB_URL = "https://github.com/opensourceways/mailman/raw/main/templates/{}/{}"
-    QUERY_SQL = "select count(*) as count from template where name={} and context={}"
-    UPDATE_SQL = "update template set uri={} where name={} and context={}"
-    INSERT_SQL = """insert into template(name, context, uri, username) values({}, {}, {}, NULL);"""
+    QUERY_SQL = """select count(*) as count from template where name='{}' and context='{}'"""
+    UPDATE_SQL = "update template set uri='{}' where name='{}' and context='{}'"
+    INSERT_SQL = """insert into template(name, context, uri, username) values('{}', '{}', '{}', NULL);"""
 
 
 def get_template_name_list(domain_name):
@@ -28,30 +29,35 @@ def refresh_to_mysql(template_name_list, domain, username, password, ip, port, d
     try:
         for template_name in template_name_list:
             complete_git_url = GlobalConfig.GITHUB_URL.format(domain, template_name)
-            base_name = os.path.basename(template_name)
+            base_name = os.path.splitext(template_name)[0]
             name = ":".join(base_name.split("_"))
             query_sql = GlobalConfig.QUERY_SQL.format(name, domain)
             cursor.execute(query_sql)
             data = cursor.fetchone()
-            if data["count"] != 0:
+            if data[0] != 0:
                 update_complete_sql = GlobalConfig.UPDATE_SQL.format(complete_git_url, name, domain)
                 cursor.execute(update_complete_sql)
             else:
                 insert_complete_sql = GlobalConfig.INSERT_SQL.format(name, domain, complete_git_url)
                 cursor.execute(insert_complete_sql)
+        mysql_conn.commit()
     except Exception as e:
-        print(e)
+        print(e, traceback.format_exc())
     finally:
         mysql_conn.close()
 
 
 @click.command()
-@click.option("--domain", help="The name of domain, eg:lists.osinfra.cn")
-@click.option("--username", help="The username of mysql")
-@click.option("--password", help="The password of mysql")
-@click.option("--ip", help="The ip of mysql")
-@click.option("--port", default=3306, help="The port of mysql")
+@click.argument("domain")
+@click.argument("username")
+@click.argument("password")
+@click.argument("ip")
+@click.argument("port", default=3306)
 def refresh_data(domain, username, password, ip, port):
     """获取所有的数据，先查询数据库是否存在，如果存在，则修改，否则新填"""
     template_name_list = get_template_name_list(domain)
     refresh_to_mysql(template_name_list, domain, username, password, ip, port)
+
+
+if __name__ == '__main__':
+    refresh_data()
